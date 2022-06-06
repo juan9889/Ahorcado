72eea35080e4c70846a6adae416c5a0f92544d7e
juan <juan.fogliato@gmail.com>
2022-06-06 20:18:09 -0300

    clase usuario

--- Ahorcado.Entidades/Usuario.cs
+++ Ahorcado.Entidades/Usuario.cs
@@ -1,6 +1,8 @@
 ﻿namespace Ahorcado.Entidades;
-public class Class1
+public class Usuario
 {
-
+    public int ID { get; set; }
+    public string Nombre { get; set; }
+    public string Clave { get; set; }
 }
 
