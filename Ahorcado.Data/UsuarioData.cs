c2a012f5ed3202d748dd3073f8c898ab74d7a830
francoluna <franco.luna@sked24.com>
2022-06-28 21:50:16 -0300

    added validations to login and register

--- Ahorcado.Data/UsuarioData.cs
+++ Ahorcado.Data/UsuarioData.cs
@@ -10,52 +10,47 @@ public class UsuarioData : Context
 {
     public static Usuario GetUsuario(long id)
     {
-        string name = "";
         Usuario usuario = new();
-        using (SqliteConnection connection = new SqliteConnection(getConnectionString()))
+        using (SqliteConnection connection = new(getConnectionString()))
         {
             connection.Open();
 
             var command = connection.CreateCommand();
             command.CommandText = "SELECT * FROM usuarios WHERE id = $id";
             command.Parameters.AddWithValue("$id", id);
-            
-            using (var reader = command.ExecuteReader())
+
+            using SqliteDataReader reader = command.ExecuteReader();
+            if (reader.Read())
             {
-                if (reader.Read())
-                {
-                    usuario.ID= reader.GetInt32(0);
-                    usuario.Nombre = reader.GetString(1);
-                    usuario.Clave = reader.GetString(2);
-                }
+                usuario.ID = reader.GetInt32(0);
+                usuario.Nombre = reader.GetString(1);
+                usuario.Clave = reader.GetString(2);
             }
         }
 
         return usuario;
     }
-    
+
 
     public static Usuario Login(string nombre, string clave)
     {
         Usuario usuario = new();
-        
-        using (SqliteConnection connection = new SqliteConnection(getConnectionString()))
+
+        using (SqliteConnection connection = new(getConnectionString()))
         {
             connection.Open();
-            
+
             var command = connection.CreateCommand();
-            command.CommandText = "SELECT * FROM usuarios WHERE nombre = $nombre AND pass = $clave" ;
+            command.CommandText = "SELECT * FROM usuarios WHERE nombre = $nombre AND pass = $clave";
             command.Parameters.AddWithValue("$nombre", nombre);
             command.Parameters.AddWithValue("$clave", clave);
 
-            using (var reader = command.ExecuteReader())
+            using SqliteDataReader reader = command.ExecuteReader();
+            if (reader.Read())
             {
-               if (reader.Read())
-                {
-                    usuario.ID= reader.GetInt32(0);
-                    usuario.Nombre = reader.GetString(1);
-                    usuario.Clave = reader.GetString(2);
-                }
+                usuario.ID = reader.GetInt32(0);
+                usuario.Nombre = reader.GetString(1);
+                usuario.Clave = reader.GetString(2);
             }
         }
         return usuario;
@@ -63,20 +58,14 @@ public class UsuarioData : Context
 
     public static bool RegistrarUsuario(string nombre, string clave)
     {
-        bool result = false;
-        using (SqliteConnection connection = new SqliteConnection(getConnectionString()))
-        {
-            connection.Open();
+        using SqliteConnection connection = new(getConnectionString());
+        connection.Open();
 
-            var command = connection.CreateCommand();
-            command.CommandText = "INSERT INTO usuarios(nombre, pass) VALUES($nombre, $pass)";
-            command.Parameters.AddWithValue("$nombre", nombre);
-            command.Parameters.AddWithValue("$pass", clave);
+        var command = connection.CreateCommand();
+        command.CommandText = "INSERT INTO usuarios(nombre, pass) VALUES($nombre, $pass)";
+        command.Parameters.AddWithValue("$nombre", nombre);
+        command.Parameters.AddWithValue("$pass", clave);
 
-            command.ExecuteNonQuery();
-         }
-        
-        result = true;
-        return result;
+        return command.ExecuteNonQuery() > 0; 
     }
 }
