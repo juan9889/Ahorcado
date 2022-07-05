51af5b8a3834833e6f32064bcedd70e9ec439f1f
juan <juan.fogliato@gmail.com>
2022-07-05 12:29:04 -0300

    Revert "string conexion para servidor"
    This reverts commit 97e1c11dc85dc95e1a596d8ced9b8607c723b095.

--- Ahorcado.Data/Context.cs
+++ Ahorcado.Data/Context.cs
@@ -11,19 +11,13 @@ namespace Ahorcado.Data
         public static string getConnectionString()
         {
             string bar = "/";
-            string connectionstring = "";
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 bar = "\\";
             }
             var currentDirectory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
             string projectDirectory = currentDirectory.Parent.Parent.Parent.Parent.FullName;
-            connectionstring = "Data Source=" + projectDirectory + bar + "AhorcadoDB.db";
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                connectionstring = "Data Source=AhorcadoDB.db";
-            }
-            return connectionstring;
+            return "Data Source=" + projectDirectory + bar + "AhorcadoDB.db";
 
         }
     }
