97e1c11dc85dc95e1a596d8ced9b8607c723b095
juan <juan.fogliato@gmail.com>
2022-07-05 12:25:09 -0300

    string conexion para servidor

--- Ahorcado.Data/Context.cs
+++ Ahorcado.Data/Context.cs
@@ -11,13 +11,19 @@ namespace Ahorcado.Data
         public static string getConnectionString()
         {
             string bar = "/";
+            string connectionstring = "";
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 bar = "\\";
             }
             var currentDirectory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
             string projectDirectory = currentDirectory.Parent.Parent.Parent.Parent.FullName;
-            return "Data Source=" + projectDirectory + bar + "AhorcadoDB.db";
+            connectionstring = "Data Source=" + projectDirectory + bar + "AhorcadoDB.db";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                connectionstring = "Data Source=AhorcadoDB.db";
+            }
+            return connectionstring;
 
         }
     }
